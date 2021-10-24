8a9e03dc6008a0863d84fc0c676dfdf8df54b6eb
Joakim Andersson <joakim.andersson@docusign.com>
2021-10-24 15:46:20 +0200

    Add custom configs

---
+++ lua/custom/lspconfig.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+
+   -- lspservers with default config
+
+   local servers = { "pyright" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+         -- root_dir = vim.loop.cwd,
+         flags = {
+            debounce_text_changes = 150,
+         },
+      }
+   end
+
+  -- lspservers with special config
+  -- nvim_lsp.pyright.setup {
+  -- on_init = function(client)
+  --   client.config.settings.python.analysis.extrapaths = { "tests" }
+  --   client.notify("workspace/didChangeConfiguration")
+  --   return true
+  -- end
+-- }
+end
+return M
